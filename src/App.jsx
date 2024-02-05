0372b043b71a63ab9ae26ea4e11a1e441ab06c16
Andrei Harenkov <a@harenkon.ru>
2024-02-06 00:32:48 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import StoreClass from "./StoreClass";
+
+function App() {
+  return (
+    <div className="container">
+      <StoreClass />
+    </div>
+  );
+}
+
+export default App;
