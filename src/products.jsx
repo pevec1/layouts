f0e64ddc87549b08d858a1e3f3069a4a3cc8c479
Andrei Harenkov <a@harenkon.ru>
2024-02-08 13:33:39 +0300

    Revert "setup"
    This reverts commit e93d36bd3a5cd86ed13f16251a57fba4b60d97a6.

--- src/products.jsx
+++ src/products.jsx
@@ -3,37 +3,37 @@ const products = [
     name: "Nike Metcon 2",
     price: "130",
     color: "red",
-    img: "layouts/src/img/1.jpg",
+    img: "../src/img/1.jpg",
   },
   {
     name: "Nike Metcon 2",
     price: "130",
     color: "green",
-    img: "layouts/src/img/2.jpg",
+    img: "../src/img/2.jpg",
   },
   {
     name: "Nike Metcon 2",
     price: "130",
     color: "blue",
-    img: "layouts/src/img/3.jpg",
+    img: "../src/img/3.jpg",
   },
   {
     name: "Nike Metcon 2",
     price: "130",
     color: "black",
-    img: "layouts/src/img/4.jpg",
+    img: "../src/img/4.jpg",
   },
   {
     name: "Nike free run",
     price: "170",
     color: "black",
-    img: "layouts/src/img/7.jpg",
+    img: "../src/img/7.jpg",
   },
   {
     name: "Nike Metcon 3",
     price: "150",
     color: "green",
-    img: "layouts/src/img/5.jpg",
+    img: "../src/img/5.jpg",
   },
 ];
 
